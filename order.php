f80c028bd47e12bead7c08ef12397adc6dbb9610
Patricia <107477504+PGeddie@users.noreply.github.com>
2023-06-19 10:43:25 +0200

    Update order.php

--- order.php
+++ order.php
@@ -6,13 +6,14 @@ $host = "localhost";
 	$conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
 	
 $name = $_POST['orderNum'];
+$name = $_POST['order];
 $name = $_POST['fullName'];
 $name = $_POST['contactNum'];
 $name = $_POST['email'];
 $name = $_POST['address'];
 
 $stmt = $pdo ->prepare("INSERT INTO orders (orderNum, fullName, contactNum, email, address) VALUES (?, ?, ?, ?, ?)");
-$stmt -> execute([$_POST['orderNum'], $_POST['fullName'], $_POST['contactNum'], $_POST['email'], $_POST['address']);
+$stmt -> execute([$_POST['orderNum'], $_POST['order], $_POST['fullName'], $_POST['contactNum'], $_POST['email'], $_POST['address']);
  if ($stmt) {
 	 echo 'Information has been posted.';
  } else {
